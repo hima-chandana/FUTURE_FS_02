eaac8cf920fb11b99fa2c61543f1a9bdce76ce74
HIMA CHANDANA <bhoomireddyhima@gmail.com>
2026-06-27 13:24:35 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    base: command === 'serve' ? '/' : '/FUTURE_FS_02/',
+  }
 })
